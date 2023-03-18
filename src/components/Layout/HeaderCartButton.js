6c7b9b9ad5f19e2516bfe8a486969c1c4a276c2a
DevAntonioRogers <ar919061493@gmail.com>
2023-03-17 20:28:44 -0400

    Added a cart button animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,11 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+
+  const [btnHighlighted, setBtnHighlighted] = useState(false)
 
   const cartCtx = useContext(CartContext);
 
@@ -11,8 +13,27 @@ const HeaderCartButton = (props) => {
     return curNumber + item.amount
   }, 0)
 
+  const { items } = cartCtx
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return
+    }
+    setBtnHighlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timer)
+    }
+
+  }, [items])
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
